6941c40950b99dc56a615391db440f2261a49fe5
1984MRB <fraguel_rock22@hotmail.com>
2019-03-06 09:38:06 +0000

    First commit

---
+++ application.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask, render_template,send_from_directory
+from flask_bootstrap import Bootstrap
+
+template_folder = os.path.join(os.path.dirname(__file__),'templates')
+static_folder = os.path.join(os.path.dirname(__file__),'static')
+app = Flask(__name__, template_folder = template_folder, static_folder=static_folder )
+
+Bootstrap(app)
+
+@app.route('/')
+def app_entrypoint():
+    return render_template('index.html')
+
+@app.route('/css/<path:filename>')
+def serve_css(filename):
+    return send_from_directory('static/css', filename)
+
+@app.route('/imgs/<path:filename>')
+def serve_imgs(filename):
+    return send_from_directory('static/imgs', filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
